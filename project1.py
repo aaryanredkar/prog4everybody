495df317fb14aa16be9350ca42e3fd25b2582b4e
aaryanredkar <aaryantoki@live.com>
2015-12-22 09:50:45 -0800

    tells the letters in your name

--- project1.py
+++ project1.py
@@ -1,4 +1,4 @@
+
 name = input("Please enter your name: ")
-print("Hello, "+name + "!")
-print("Your name has the letters " " + ".")
-print("Your name is %s characters long. " %len(name))
+for i in name:
+   print (i)
