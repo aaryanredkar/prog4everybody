f9bbd5f6e1cc46145e2efa5a4715d30d0110e4ce
aaryanredkar <aaryantoki@live.com>
2016-01-25 16:20:50 -0800

    program for your name

---
+++ nameent.py
@@ -0,0 +1,5 @@
+name = input("What is your name:")
+num= int(input("How many times do you want your name to be printed:"))
+fname= name*num
+space = (" ")
+print (" "+fname + )
---
+++ nameentnormal.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+print("Hello, "+name + "!")
+print("Let me print you name %s times. " %len(name))
+for i in name:
+   print (name)
