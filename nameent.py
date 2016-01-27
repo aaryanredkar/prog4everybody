3c8e27d4d6908ee6a2c75ab72a173d9afc1ac5ef
aaryanredkar <aaryantoki@live.com>
2016-01-26 18:45:31 -0800

    print name certain number of times

--- nameent.py
+++ nameent.py
@@ -1,5 +1,12 @@
+def print_name(user_name,number_of_times):
+    for i in range (1,number_of_times + 1):
+        print (user_name)
 name = input("What is your name:")
 num= int(input("How many times do you want your name to be printed:"))
-fname= name*num
-space = (" ")
-print (" "+fname + )
+#fname= name*num + " "
+#space = (" ")
+#print (fname)
+print_name(name, num)
+
+
+
